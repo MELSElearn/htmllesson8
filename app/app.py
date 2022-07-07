4a004335c52a56bdc4a2551f8bb0ee980cd3e275
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2022-07-07 12:35:56 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,18 +10,34 @@ app = Flask(__name__)
 def hello_world():
     request_type_str = request.method
     if request_type_str == 'GET':
-        return render_template('index.html', href2='static/none.png')
+        return render_template('index.html', href2='static/none.png', href3='')
     else:
         myage = request.form['age']
         mygender = request.form['gender']
+        mybread = ''
         if str(myage) =='' or str(mygender) =='':
-            return render_template('index.html', href2='static/none.png')
+            return render_template('index.html', href2='static/none.png', href3='Please insert your age and gender.')
         else:
             model = load('app/bread-recommender.joblib')
             np_arr = np.array([myage, mygender])
             predictions = model.predict([np_arr])  
             predictions_to_str = str(predictions)
-            #return predictions_to_str
-            return render_template('index.html', href2='static/'+predictions_to_str+'.png')
+            
+            if predictions_to_str == 'donut':
+                mybread = 'static/donut.png'
+            elif predictions_to_str == 'croissant':
+                mybread = 'static/croissant.png'
+            elif predictions_to_str == 'whole grain':
+                mybread = 'static/wholegrain.png'
+            elif predictions_to_str == 'wheat bread':
+                mybread = 'static/wheatbread.png'
+            elif predictions_to_str == 'swiss roll':
+                mybread = 'static/swissroll.png'
+            elif predictions_to_str == 'sandwich':
+                mybread = 'static/sandwich.png'
+            else:
+                mybread = 'static/none.png' 
+                
+            return render_template('index.html', href2=str(mybread), href2='The suitable bread for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
         
 
